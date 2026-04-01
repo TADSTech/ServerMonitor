84250943a809a1e95751279fa063e362f43e2de8
Michael Tunwashe <motrenewed@gmail.com>
2026-04-01 20:34:03 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import http
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+ServerUrl
+def getServer():
+    return
+
+def main():
+    print("Hello, World!")
+    
+    
+if __name__ == "__main__":
+    main()
